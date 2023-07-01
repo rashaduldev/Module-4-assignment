137305e4674b7c0f6e1bb3a7431281f63ba82c80
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-07-01 12:31:58 +0600

    Again Done

--- Again move -3/success/index.js
+++ Again move -3/success/index.js
@@ -1,16 +1,10 @@
-// Create a function to be called when the button is clicked
-function showText() {
-    // Get the input element by its ID
-    var input = document.getElementById('myInput');
-    
-    // Get the paragraph element by its ID
-    var paragraph = document.getElementById('myParagraph');
-    
-    // Set the text content of the paragraph to the input value
-    paragraph.textContent = input.value;
-  }
-  
-  // Attach an event listener to the button
-  var button = document.getElementById('myButton');
-  button.addEventListener('click', showText);
-  
+
+function successreq(){
+    var input=document.getElementById('mgsOutput');
+    var output=document.getElementById('INPUT');
+
+    input.textContent=output.value;
+
+}
+var submitButton=document.getElementById('clButton');
+submitButton.addEventListener('click',successreq);
