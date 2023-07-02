87188e0789278b728528f0d542082f7fc616e071
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-07-02 10:51:59 +0600

    Update product.js

--- Final-Project/product.js
+++ Final-Project/product.js
@@ -1,5 +1,5 @@
 const products = [
-    { id: 1, name: 'Product 1', price: 10 },
+    { id: 1, name: 'Yeamaha', price: 10 },
     { id: 2, name: 'Product 2', price: 15 },
     { id: 3, name: 'Product 3', price: 5 }
 ];
